83e460fb4276840e323e4d2665f5430290d171f2
unknown <breid@BREID01.myfamily.int>
2012-11-20 07:59:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '186fc01afd9d4dad8ba9937986c2f2aee23d93b98c8d95d4fd1e4765228b49e539661ab04611bf8ee615897cce190cabdfbb14e658492b4bc739f998cc2b4095'
